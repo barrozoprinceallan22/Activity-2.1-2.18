c259c32bff2937ada7dbbc58e29fa0703bf275f6
barrozoprinceallan22 <barrozoprinceallan22@gmail.com>
2026-09-04 11:34:13 +0800

    Activity 2.9 Room.java

---
+++ Room.java
@@ -0,0 +1,14 @@
+/*Prince Allan Barrozo
+BSIT NETSEC 1-1*/
+public class Room 
+{
+    public static void main(String[] args) {
+        int length = 20;
+        int width = 15;
+        int floorSpace;
+        
+        floorSpace = length * width;
+        
+        System.out.println("The Floor Space is " + floorSpace + " square feet. ");
+    }
+}

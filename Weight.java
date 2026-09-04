549b071188e069df6463d5f2be404514086acace
barrozoprinceallan22 <barrozoprinceallan22@gmail.com>
2026-09-04 11:32:55 +0800

    Activity 2.8 Weight.java

---
+++ Weight.java
@@ -0,0 +1,12 @@
+/*Prince Allan Barrozo
+BSIT NETSEC 1-1*/
+public class Weight
+{
+    public static void main(String[] args) {
+        double pounds = 20;
+        double kilograms;
+        
+        kilograms = pounds * 0.454;
+        System.out.println(pounds + " pounds is " + kilograms);
+    }
+}

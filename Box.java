7f9dd5cd2d002601d36e34604a4c8166fdb0c38c
barrozoprinceallan22 <barrozoprinceallan22@gmail.com>
2026-09-04 12:04:04 +0800

    Activity 2.17  Box.java

---
+++ Box.java
@@ -0,0 +1,14 @@
+/*Prince Allan Barrozo
+BSIT NETSEC 1-1*/
+public class Box {
+    public static void main(String[] args) {
+
+        System.out.println("*************         *");
+        System.out.println("*           *        * *");
+        System.out.println("*           *       *   *");
+        System.out.println("*           *      *     *");
+        System.out.println("*           *       *   *");
+        System.out.println("*           *        * *");
+        System.out.println("*************         *");
+    }
+}

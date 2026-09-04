96582f4ec1d2d49b4a215244633442523bfb6e9c
barrozoprinceallan22 <barrozoprinceallan22@gmail.com>
2026-09-04 11:58:43 +0800

    Activity 2.15 Escape.java

---
+++ Escape.java
@@ -0,0 +1,9 @@
+/*Prince Allan Barrozo
+BSIT NETSEC*/
+public class Escape {
+    public static void main(String[] args) {
+
+        System.out.println("First Name\tLast Name\tStudent Number\tPhone Number");
+        System.out.println("Prince\t\tBarrozo\t\t26-350\t\t09127866186");
+    }
+}

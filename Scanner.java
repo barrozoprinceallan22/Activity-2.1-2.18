bc8f356d5c47c100844844e044a54b44758d9b36
barrozoprinceallan22 <barrozoprinceallan22@gmail.com>
2026-09-04 11:40:28 +0800

    Activity 2.13 Scanner.java

---
+++ Scanner.java
@@ -0,0 +1,19 @@
+/*Prince Allan Barrozo
+BSIT NETSEC 1-1*/
+import java.util.Scanner;
+
+public class Sum {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter first integer: ");
+        int num1 = input.nextInt();
+
+        System.out.print("Enter second integer: ");
+        int num2 = input.nextInt();
+
+        int sum = num1 + num2;
+
+        System.out.println("The sum is: " + sum);
+    }
+}
